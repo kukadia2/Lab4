d9278fc08351b7ee02c13fd539e0347a04bdb0f5
Your Name <you@example.com>
2017-09-26 12:04:12 -0500

    Finished

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,47 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * A class that teaches you how to write clean code.
+ * @author kukad
+ */
+public class ReclamationProject {
+    /**
+     * @param a
+     * a the first String
+     * @param b
+     * b the second String
+     * @return r
+     */
+    static String doIt(final String a, final String b) {
+        if (a.length() > b.length()) {
+            String c = a;
+        }
+        String r = ""; // I love the ternary operator!
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                }
+            } // Ah yeah
+        }
+        return r;
+     }
+
+    /**
+     * Testing outputs.
+     * @param args
+     * args the args
+     */
+    public static void main(final String[] args) {
+        String one = "testing";
+        String two = "a string";
+        System.out.println(doIt(one, two));
+    }
+
 }
